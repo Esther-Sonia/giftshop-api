337107ed08a3854b5dedd16c4b8d6d14dab2142a
Esther-Sonia <esthersonia21@gmail.com>
2025-05-01 16:58:11 +0300

    fix changes

--- server.js
+++ server.js
@@ -1,34 +1,14 @@
-// server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db/db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
+const cors = require('cors');
 
-// Use default middlewares (logger, static, cors, etc)
+server.use(cors());
 server.use(middlewares);
-
-// Add custom routes before JSON Server router
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-  '/blog/:resource/:id/show': '/:resource/:id'
-}));
-
-// To handle POST, PUT and PATCH you need to use a body-parser
-server.use(jsonServer.bodyParser);
-
-// Custom middleware for authentication (example)
-server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now();
-  }
-  // Continue to JSON Server router
-  next();
-});
-
-// Use the router
 server.use(router);
 
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
